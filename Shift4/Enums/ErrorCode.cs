748e12becc3d82fde54cdd159b482f6431c5270d
Krzysztof Bieroń <7493083+el-dot@users.noreply.github.com>
2024-01-03 12:48:59 +0100

    Add new values to ErrorCode enum

--- Shift4/Enums/ErrorCode.cs
+++ Shift4/Enums/ErrorCode.cs
@@ -8,54 +8,85 @@ namespace Shift4.Enums
 {
     public enum ErrorCode
     {
-        [EnumMember(Value = "no_error")]
-        NoError,
+
+        [EnumMember(Value = "amount_too_high")]
+        AmountTooHigh,
+
+        [EnumMember(Value = "amount_too_low")]
+        AmountTooLow,
 
         [EnumMember(Value = "invalid_number")]
         InvalidNumber,
 
-	    [EnumMember(Value = "invalid_expiry_month")]
+        [EnumMember(Value = "invalid_expiry_month")]
         InvalidExpiryMonth,
 
-	    [EnumMember(Value = "invalid_expiry_year")]
+        [EnumMember(Value = "invalid_expiry_year")]
         InvalidExpiryYear,
 
-	    [EnumMember(Value = "invalid_cvc")]
+        [EnumMember(Value = "invalid_cvc")]
         InvalidCvc,
 
-	    [EnumMember(Value = "incorrect_cvc")]
+        [EnumMember(Value = "incorrect_cvc")]
         IncorrectCvc,
 
-	    [EnumMember(Value = "incorrect_zip")]
+        [EnumMember(Value = "incorrect_zip")]
         IncorrectZip,
 
-	    [EnumMember(Value = "expired_card")]
+        [EnumMember(Value = "incorrect_address")]
+        IncorrectAddress,
+
+        [EnumMember(Value = "expired_card")]
         ExpiredCard,
 
-	    [EnumMember(Value = "insufficient_funds")]
+        [EnumMember(Value = "insufficient_funds")]
         InsufficientFunds,
 
-	    [EnumMember(Value = "lost_or_stolen")]
+        [EnumMember(Value = "lost_or_stolen")]
         LostOrStolen,
 
-	    [EnumMember(Value = "suspected_fraud")]
+        [EnumMember(Value = "suspected_fraud")]
         SuspectedFraud,
 
-	    [EnumMember(Value = "card_declined")]
+        [EnumMember(Value = "card_declined")]
         CardDeclined,
 
-	    [EnumMember(Value = "processing_error")]
+        [EnumMember(Value = "brand_not_supported")]
+        BrandNotSupported,
+
+        [EnumMember(Value = "currency_not_supported")]
+        CurrencyNotSupported,
+
+        [EnumMember(Value = "processing_error")]
         ProcessingError,
 
-	    [EnumMember(Value = "blacklisted")]
+        [EnumMember(Value = "blacklisted")]
         Blacklisted,
+
+        [EnumMember(Value = "authentication_required")]
+        AuthenticationRequired,
+
+        [EnumMember(Value = "blocked")]
+        Blocked,
+
+        [EnumMember(Value = "do_not_try_again")]
+        DoNotTryAgain,
 
         [EnumMember(Value = "expired_token")]
         ExpiredToken,
 
-	    /**
-	     * Used when received value can't be mapped to this enumeration.
-	     */
-	    Unrecognized
+        [EnumMember(Value = "limit_exceeded")]
+        LimitExceeded,
+
+        [EnumMember(Value = "payment_method_declined")]
+        PaymentMethodDeclined,
+
+        [EnumMember(Value = "invalid_account")]
+        InvalidAccount,
+
+        /**
+         * Used when received value can't be mapped to this enumeration.
+         */
+        Unrecognized
     }
 }
