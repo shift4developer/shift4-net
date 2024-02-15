e7cc61168079504b3b01fd61b553bdb046371630
Jarosław Mickiewicz <jaroslaw.mickiewicz@securionpay.com>
2024-02-15 15:05:06 -0800

    Add Payout and PayoutTransactions to sdk

---
+++ Shift4/Request/PayoutListRequest.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Shift4.Request
+{
+    public class PayoutListRequest : ListRequest
+    {
+    }
+}
---
+++ Shift4/Response/PayoutTransaction.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using Shift4.Converters;
+using Shift4.Enums;
+using System;
+using System.Collections.Generic;
+
+namespace Shift4.Response
+{
+    public class PayoutTransaction : BaseResponse
+    {
+       [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("created")]
+        [JsonConverter(typeof(DateConverter))]
+        public DateTime Created { get; set; }
+
+        [JsonProperty("type")]
+        [JsonConverter(typeof(SafeEnumConverter))]
+        public PayoutTransactionType Type { get; set; }
+
+        [JsonProperty("amount")]
+        public int Amount { get; set; }
+
+        [JsonProperty("fee")]
+        public int? Fee { get; set; }
+
+        [JsonProperty("currency")]
+        public string Currency { get; set; }
+
+        [JsonProperty("source")]
+        public string Source { get; set; }
+
+        [JsonProperty("payout")]
+        public string Payout { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("exchangeRate")]
+        public double? ExchangeRate { get; set; }
+    }
+}
+
